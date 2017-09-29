e6e1d9edf56b29f37bbafa7694838544d45d1fe0
arsen4ik98 <arsenbeglaryan98@gmail.com>
2017-09-29 14:48:22 +0300

    сделал тесты

--- QueueRealisation_Test/Class1.cs
+++ QueueRealisation_Test/Class1.cs
@@ -12,7 +12,16 @@ namespace QueueRealisation_Test
     public class Class1
     {
         [Test()]
-        public void asd()
+        [ExpectedException(typeof(Exception))]
+        public void Test1()
+        {
+            QueueRealisation<int> asd = new QueueRealisation<int>();
+            int act = asd.Pop();
+         
+
+        }
+        [Test()]
+        public void Test_push3_pop1()
         {
             QueueRealisation<int> asd = new QueueRealisation<int>();
             asd.Push(1);
@@ -22,6 +31,63 @@ namespace QueueRealisation_Test
             asd.Pop();
             int act = asd.Pop();
             Assert.AreEqual(expec, act);
+
+        }
+        [Test()]
+        public void Test_count1()
+        {
+            QueueRealisation<int> asd = new QueueRealisation<int>();
+            asd.Push(8);
+            int expec = 1;
+            int act = asd.count;
+            Assert.AreEqual(expec, act);
+        }
+        [Test()]
+        public void Test_count2()
+        {
+            QueueRealisation<int> asd = new QueueRealisation<int>();
+            
+            asd.Push(1);
+            asd.Push(2);
+            int expec = 2;
+            int act = asd.count;
+            Assert.AreEqual(expec, act);
+        }
+        [Test()]
+        public void Test_push2_pop1()
+        {
+            QueueRealisation<int> asd = new QueueRealisation<int>();
+
+            asd.Push(1);
+            asd.Push(2);
+            asd.Pop();
+            int expec = 1;
+            int act = asd.count;
+            Assert.AreEqual(expec, act);
+        }
+        [Test()]
+        public void Test_pus_pop_check()
+        {
+            QueueRealisation<int> asd = new QueueRealisation<int>();
+            asd.Push(4);
+            asd.Push(2);
+            asd.Push(6);
+            int expec = 4;
+            int act = asd.TakeTop();
+            Assert.AreEqual(expec, act);
+            asd.Pop();
+            expec = 2;
+            act = asd.TakeTop();
+            Assert.AreEqual(expec, act);
+            asd.Pop();
+            expec = 6;
+            act = asd.TakeTop();
+            Assert.AreEqual(expec, act);
+            asd.Pop();
+            expec = 0;
+            act = asd.TakeTop();
+            Assert.AreEqual(expec, act);
+            
         }
     }
 }
